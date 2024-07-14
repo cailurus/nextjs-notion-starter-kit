b9add3a4a473d93b459efc2ec9a63778b9ea5ab4
More Z <pandachow19910411@gmail.com>
2024-07-15 04:31:32 +0800

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -187,7 +187,8 @@ export const NotionPage: React.FC<types.PageProps> = ({
   // const isRootPage =
   //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
   const isBlogPost = block?.type === 'page' && block?.parent_table === 'collection'
-  const showTableOfContents = !!isBlogPost
+  //const showTableOfContents = !!isBlogPost
+  const showTableOfContents = true
   const minTableOfContentsItems = 2
 
   const pageAside = React.useMemo(
