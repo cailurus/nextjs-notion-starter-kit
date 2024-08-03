24dde0d0f073839d3b359dadfdb555074cdb228c
More Z. <pandachow19910411@gmail.com>
2024-08-04 00:19:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  // navigationStyle: 'default',
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'About',
-      pageId: '8d88ac05ada64359bebe28bcb774d95c'
-    }
+  navigationStyle: 'default',
+  //navigationStyle: 'custom',
+  //navigationLinks: [
+  //  {
+  //    title: 'About',
+  //    pageId: '8d88ac05ada64359bebe28bcb774d95c'
+  //  }
   //   {
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
   //   }
-  ]
+  //]
 })
